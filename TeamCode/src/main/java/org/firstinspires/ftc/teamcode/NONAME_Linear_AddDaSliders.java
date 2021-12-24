959e9c57e0b5f59a9944a3b1465c993907ec5dfd
MoonstreamInSpace <isaac.saff@gmail.com>
2021-12-24 11:00:33 -0500

    COMMENTS COMMENTS COMMENTS COMMENTS COMMENTS COMMENTS COMMENTS COMMENTS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NONAME_Linear_AddDaSliders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NONAME_Linear_AddDaSliders.java
@@ -9,114 +9,75 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-//This is a system where the triggers control the speed of the bot. For example, if you have the left stick at full forward and the left trigger half pressed in, you get half power.
+//NOTREADY <- you may see comments like this in the code. These represent code that would cause errors now that needs a specific fix. For example, the spinner isn't defined on or attached to the robot, so I leave it off for now.
 @SuppressWarnings("FieldCanBeLocal")
 @TeleOp(name="NONAME Op Mode - ARM",group="Linear")
 //@Disabled
 public class NONAME_Linear_AddDaSliders extends LinearOpMode {
-
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    //ldrive is left front drive
-    //rdrive is right front drive
-    //lbd is left back drive
-    //rbd is right back drive
-    private DcMotor ldrive = null;
-    private DcMotor rdrive = null;
-    private DcMotor drum = null;
-    private DcMotor arm=null;
-    //private DcMotor ringLift = null;
-    //private Servo door = null;
-    //Currently, all servo positions must remain on
-    //the interval [0.13,0.87], or the servos will not respond.
-    private final double lowPowerMod = 0.35;
-    private final double highPowerMod = 0.7;
-    private final double turnReductionLowPower = 0.80;
-    private double armPosA=ArmRef.posA;
+    //Variable Initializations
+    //Note: anything involving ArmRef.<something> is pulling variables from the ArmRef code. I put those variables there for ease of access, and also to allow FTCDashboard to access them.
+    private ElapsedTime runtime = new ElapsedTime(); //Keeps track of the time during the code.
+    private DcMotor ldrive = null; //Left tread motor
+    private DcMotor rdrive = null;  //Right tread motor
+    private DcMotor drum = null; //Drum motor
+    private DcMotor arm=null; //Arm motor
+    //NOTREADY private DcMotor spinner=null; //Carousel spinner motor
+    private double armPosA=ArmRef.posA; //Setting arm positions based on the ArmRef class (I set them there mostly for ease of access and also because that's where FTCDashboard can access them)
     private double armPosB=ArmRef.posB;
     private double armPosX=ArmRef.posX;
     private  double armPosY=ArmRef.posY;
-    private boolean autoArm=true;
-    private double armError=ArmRef.PowerMode.armError;
-    private double tgtArmPower=ArmRef.PowerMode.tgtArmPower;
-    private double targetArmPos=0;
-    private boolean rightStrafe = false;
-    private double modulation = 0;
-    private boolean lowPower = false;
-    private boolean xlock=false;
-    public static double armDistMultiplier=ArmRef.PowerMode.armDistMultiplier;
-    public static double armPowerLimit=ArmRef.PowerMode.armPowerLimit;
+    private boolean autoArm=true; //This is a debug variable, if it is false, then the arm motor shuts off, allowing for manual repositioning of the arm.
+    private double armError=ArmRef.PowerMode.armError; //This is the acceptable error range within which the arm can be of the target position. It turned out to be not useful.
+    private double tgtArmPower=ArmRef.PowerMode.tgtArmPower; //This is the base power used in the power control equation.
+    private double targetArmPos=0; //Target arm position, in encoder pulses.
+    private double modulation = 0.7; //Multiplier for how much the motor power should be reduced.
+    private boolean xlock=false; //This is a variable used to detect if X is being held, explained later.
+    public static double armDistMultiplier=ArmRef.PowerMode.armDistMultiplier; //This is actually a divisor for arm power, explained later.
+    public static double armPowerLimit=ArmRef.PowerMode.armPowerLimit; //This limits the max arm power, which is good for preventing the arm from slamming into things or moving too quickly.
 
     @Override
     public void runOpMode() {
-        FtcDashboard dashboard = FtcDashboard.getInstance();
+        FtcDashboard dashboard = FtcDashboard.getInstance(); //Initializing Dashboard
         Telemetry dashboardTelemetry = dashboard.getTelemetry();
-        //telemetry.addData("Status", "Initialized");
-        //telemetry.update();
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        ldrive  = hardwareMap.get(DcMotor.class, HardwareReference.LEFT_DRIVE);
+        ldrive  = hardwareMap.get(DcMotor.class, HardwareReference.LEFT_DRIVE); //Initializing all the motors with the names from the HardwareReference.
         rdrive = hardwareMap.get(DcMotor.class, HardwareReference.RIGHT_DRIVE);
         arm = hardwareMap.get(DcMotor.class, HardwareReference.ARM);
         drum = hardwareMap.get(DcMotor.class, HardwareReference.BAND_DRUM);
         arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         drum.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //NOTREADY spinner = hardwareMap.get(DcMotor.class, "spinner");
 
-        //foo = hardwareMap.get(DcMotor.class, "foo_motor");
-        //ringLift = hardwareMap.get(DcMotor.class, HardwareReference.RING_LIFT);
-        //door = hardwareMap.get(Servo.class,HardwareReference.DOOR_SERVO);
 
-        // Most roobts need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        ldrive.setDirection(DcMotor.Direction.REVERSE);
-        rdrive.setDirection(DcMotor.Direction.FORWARD);
+        ldrive.setDirection(DcMotor.Direction.FORWARD); //Setting motor rotation directions and initializing them.
+        rdrive.setDirection(DcMotor.Direction.REVERSE);
         ldrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rdrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         arm.setDirection(DcMotor.Direction.REVERSE);
         drum.setDirection(DcMotor.Direction.FORWARD);
-        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         drum.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //doro.setPosition(doorPos);
-        // Wiat for the game to start (driver presses PLAY)
+        //NOTREADY spinner.setDirection(DcMotorSimple.Direction.FORWARD);
+        //NOTREADY spinner.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); //This sets the arm's position to 0, to set the origin.
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double doorChange;
+            // Setup a variable for each drive wheel
             double leftPower;
             double rightPower;
-            double liftPower;
-            if (lowPower) {
-                modulation = lowPowerMod;
-            } else {
-                modulation = highPowerMod;
-            }
 
-            //Toggles the funtcion that, when active, makes the hand close when any of the bumpers or triggers are pressed. If this is off, the left bumper and trigger open the hand, and the right bumper and trigger close the hand.
-            //Toggles if the linear slides are controlled seperately or together. If on, the left stick controls the lower slide and the right stick controls the right slide. If off, both sticks control both slides.
-            if (gamepad1.left_bumper&&gamepad1.right_bumper&&gamepad1.left_stick_button) {
-                rightStrafe = false;
-            }
-            if (gamepad1.left_bumper&&gamepad1.right_bumper&&gamepad1.right_stick_button) {
-                rightStrafe = true;
-            }
-            //modluation = gamepad1.left_trigger+gamepad1.right_trigger;
-            /*if (gamepad1.left_bumper||gamepad1.right_bumper) {
-                modulation = 1;
-            }*/
-            if (gamepad2.x&&!xlock) {
+            if (gamepad2.x&&!xlock&&!gamepad2.start) { //This locks/unlocks the arm. xlock is used to keep autoArm from being toggled every update.
                 xlock=true;
                 autoArm=!autoArm;
             }
-            if (!gamepad2.x) {
+            if (!gamepad2.x&&!gamepad2.start) {
                 xlock=false;
             }
-
+            //Collecting values from ArmRef, and by extension, FTCDashboard, again to allow for live debugging without having to reset the code every time.
             armPosA=ArmRef.posA;
             armPosB=ArmRef.posB;
             armPosX=ArmRef.posX;
@@ -125,97 +86,75 @@ public class NONAME_Linear_AddDaSliders extends LinearOpMode {
             tgtArmPower=ArmRef.PowerMode.tgtArmPower;
             armDistMultiplier=ArmRef.PowerMode.armDistMultiplier;
             armPowerLimit=ArmRef.PowerMode.armPowerLimit;
-            if (gamepad1.left_bumper) {
-                lowPower = true;
-            }
-            if (gamepad1.right_bumper) {
-                lowPower = false;
-            }
-            if (gamepad1.a&&!gamepad1.start) {
+            if (gamepad1.a&&!gamepad1.start) { //If A is pressed, set the target arm position to the preset for A.
                 targetArmPos = armPosA;
             }
-            if (gamepad1.b&&!gamepad1.start) {
+            if (gamepad1.b&&!gamepad1.start) { //If B is pressed, set the target arm position to the preset for B.
                 targetArmPos = armPosB;
             }
-            if (gamepad1.x&&!gamepad1.start) {
+            if (gamepad1.x&&!gamepad1.start) { //If X is pressed, set the target arm position to the preset for X.
                 targetArmPos = armPosX;
             }
-            if (gamepad1.y&&!gamepad1.start) {
+            if (gamepad1.y&&!gamepad1.start) { //If Y is pressed, set the target arm position to the preset for Y.
                 targetArmPos = armPosY;
             }
-            if (gamepad1.left_bumper&&gamepad1.right_bumper) {
+            if (gamepad1.left_bumper&&gamepad1.right_bumper) { //If both bumpers are pressed, zero the arm positions again.
                 arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                 arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             }
-            double drumPower=0;
+            double drumPower=0; //This makes the drum controlled by the triggers.
             drumPower+=gamepad1.right_trigger;
             drumPower-=gamepad1.left_trigger;
 
-            if (autoArm) {
+            if (autoArm) { //If autoArm is on, but the arm is set to 0 power, have the motor hold its position. If autoArm is not on, have the motor not hold its position so it can be manually readjusted.
                 arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             } else {
                 arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
             }
-            // Choose to drive using either Tnak Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses baisc math to combine motions and is easier to drive straight.
-            //NOTES: gamepad1.left_stick_y increases as stick goes down.
+            //This runs the calculations for motor power based on stick position.
             double drive = -gamepad1.left_stick_y;
-            //double drive = Math.max( gamepad1.left_stick_y, Math.max(gamepad1.right_stick_y, gamepad1.right_trigger - gamepad1.left_trigger));
-            double turn;
-            double turnReduction = 1;
-            if (lowPower) {
-                turnReduction=turnReductionLowPower;
-            }
+            double turn = gamepad1.left_stick_x;
+            leftPower    = (drive - turn);
+            rightPower   = (drive + turn);
 
-            turn = (gamepad1.left_stick_x*turnReduction);  //Tunring using the left stick.
-
-            leftPower    = -(drive - turn);
-            rightPower   = -(drive + turn);
-            /*TDOO clodse = (gamepad2.right_bumper||gamepad2.left_bumper);*/
-
-
-            //Figuring out if the hand is open or closed
-
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
-
-            // Send calculated power to wheels
-            //ldrive.setPower(1);
-            //rdrive.setPower(-1);
+            //This prevents the arm from thinking it should go farther down than it can, to prevent it from pressing into the control hub.
             if (targetArmPos<0) {
                 targetArmPos=0;
             }
-            double armPower=0;
-            targetArmPos+=gamepad1.right_stick_y;
-            double distance=((targetArmPos-arm.getCurrentPosition())/armDistMultiplier)*tgtArmPower;
+
+            //This calculates the target power+direction that should be delivered to the arm motor. I'm currently in the process of writing a better version of the arm control code.
+            double armPower;
+            targetArmPos+=gamepad1.right_stick_y; //Allows for fine control of arm position using the right stick.
+            armPower=((targetArmPos-arm.getCurrentPosition())/armDistMultiplier)*tgtArmPower;
             if (autoArm) {
-                arm.setPower(Range.clip(distance, -armPowerLimit, armPowerLimit));
+                arm.setPower(Range.clip(armPower, -armPowerLimit, armPowerLimit));
             } else {
                 arm.setPower(0);
             }
+            //Set the spinner to be controlled by the bumpers.
+            /*NOTREADY double spinnerPower=0;
+            if (gamepad1.left_bumper) {
+                spinnerPower=-1;
+            }
+            if (gamepad1.right_bumper) {
+                spinnerPower=1;
+            }
+            spinner.setPower(Range.clip(spinnerPower, -1.0, 1.0));*/
+
+            //Setting all the motors' powers based on earlier variables.
+            ldrive.setPower(Range.clip((leftPower)*modulation, -1.0, 1.0));
+            rdrive.setPower(Range.clip((rightPower)*modulation, -1.0, 1.0));
+            drum.setPower(Range.clip(drumPower,-1.0,1.0));
+
+            //Sending telemetry feedback back to Dashboard.
             int armPos=arm.getCurrentPosition();
-           ldrive.setPower(Range.clip((leftPower)*modulation, -1.0, 1.0));
-           rdrive.setPower(Range.clip((rightPower)*modulation, -1.0, 1.0));
-           drum.setPower(Range.clip(drumPower,-1.0,1.0));
             dashboardTelemetry.addData("Arm", "Position: "+armPos);
             dashboardTelemetry.addData("Arm2", "Target Position: "+targetArmPos);
-            dashboardTelemetry.addData("Arm Power:",""+distance);
+            dashboardTelemetry.addData("Arm Power:",""+armPower);
             dashboardTelemetry.addData("AutoArm", "State: "+(autoArm?"True":"False"));
             dashboardTelemetry.update();
-            //ringLift.setPower(Range.clip(liftPower,-1.0,1.0));
-            //door.setPosition(doorPos);
-
-
-
-        // Show the elapsed game time and wheel power.
-        //telemetry.addData("Status", "Run Time: " + runtime.toString());
-        //telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-        //telemetry.update();
+            //TODO: Make it possible to switch telemetry to the Driver Station App.
         }
     }
 }
